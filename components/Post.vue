4065751c8d019cd7eb1b895b7dad4d74982dfa36
Tom Nagengast <tnagengast@gmail.com>
2021-06-18 15:22:59 -0700

    Add Post component and NuxtLink

---
+++ components/Post.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="">
+        <div>This is a post</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['card'],
+}
+</script>
--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="border-2 px-8 py-12 my-8 shadow-md rounded max-w-2xl m-auto">
+    <div class="border-2 px-8 py-10 my-8 shadow-md rounded max-w-2xl m-auto hover:shadow-lg cursor-pointer">
         <div class="text-sm text-center text-gray-500">{{ card.date }}</div>
-        <div class="text-2xl font-bold text-center my-4">{{ card.title }}</div>
-        <div>{{ card.summary }}</div>
-        <div>Read more</div>
+        <div class="text-2xl font-bold text-center my-4 text-gray-800">{{ card.title }}</div>
+        <div class="text-lg mb-6 text-gray-700">{{ card.summary }}</div>
+        <div class="text-center text-sm pt-3"><span class="border py-2 px-4 rounded-4xl text-gray-700">Read more</span></div>
     </div>
 </template>
 
