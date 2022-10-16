e533fe35786be1d0e79fc7efed9a2cf63a92394b
Tom Nagengast <tnagengast@gmail.com>
2022-10-16 16:58:10 -0700

    Add basic endpoint

---
+++ pages/dev/neon.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Pulling from the API
+        {{ data }}
+    </div>
+</template>
+
+<script setup>
+const {data} = await useAsyncData('count', () => $fetch('/api/count'))
+</script>
