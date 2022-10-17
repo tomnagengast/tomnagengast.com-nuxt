571c12d6a33418136a21a6cbbf08d9f1dcd42f04
Tom Nagengast <tnagengast@gmail.com>
2022-10-16 17:12:11 -0700

    Add function to query neon.tech

--- server/api/count.js
+++ server/api/count.js
@@ -1,5 +1,14 @@
-let counter = 0
-export default () => {
-  counter++
-  return JSON.stringify(counter)
+import postgres from "postgres";
+
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PROJECT_NAME} = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${PROJECT_NAME}`;
+const sql = postgres(URL, {ssl: 'require'});
+
+async function getPostgresVersion() {
+    // return sql`select version()`;
+    return sql`select * from playing_with_neon`;
+}
+
+export default async () => {
+    return JSON.stringify(await getPostgresVersion())
 }
